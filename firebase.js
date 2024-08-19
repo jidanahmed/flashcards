872c5268e12a9bd8962049b97208dece41fc9875
jidanahmed <jidanoutbox@gmail.com>
2024-08-19 16:35:12 -0400

    first commit

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDI0yiwQ25s5gGPgc8iT53MMEYhcrh6lV4",
+  authDomain: "flashcards-ad7cd.firebaseapp.com",
+  projectId: "flashcards-ad7cd",
+  storageBucket: "flashcards-ad7cd.appspot.com",
+  messagingSenderId: "6152707614",
+  appId: "1:6152707614:web:041a927aa1b6f53e86580f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
